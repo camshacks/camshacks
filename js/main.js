2dbe4faeac9c8488fbadedc4de3f36cc54935efa
isaacaddis <isaacaddis21@gmail.com>
2016-10-16 00:30:37 -0700

    Icons and Other Stuff

--- js/main.js
+++ js/main.js
@@ -2,30 +2,8 @@
 	Custom CSS File.
 	Mainly initializations and stuff :D
 */
-  $(document).ready(function(){
-
-    $('.scrollspy').scrollSpy();   
-    $(".button-collapse").sideNav();
-    $('.slider').slick({
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  autoplay: true;
-    });
+$(document).ready(function () {
+  $(".navbar-nav li.trigger-collapse a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
   });
-
-// function checkForScroll(){
-//     var startY = $('nav').height() * 2;
-//     if($(window).scrollTop() >startY)
-//     {
-//         $('nav').addClass("scrolled");
-//     }
-//     else{
-//         $('nav').removeClass("scrolled");
-//     }
-// }
-
-// if($('nav').length>0){
-//     $(window).on("scroll load resize", function(){
-//         checkForScroll();
-//     });
-// }
+});
