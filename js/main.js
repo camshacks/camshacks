b30fbd67bd193a67f686579ca9ab5a449cfbe23d
isaacaddis <isaacaddis21@gmail.com>
2016-11-02 22:44:19 -0700

    Implement Smooth Scroll

--- js/main.js
+++ js/main.js
@@ -6,4 +6,16 @@ $(document).ready(function () {
   $(".navbar-nav li.trigger-collapse a").click(function(event) {
     $(".navbar-collapse").collapse('hide');
   });
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
